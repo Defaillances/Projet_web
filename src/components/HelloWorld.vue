a911de6f8caa7b0a84ededb3a00c4843219727f1
Defaillances <mathieu.son@live.fr>
2019-10-23 15:25:02 +0200

    ajout de server.js

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,142 +1,156 @@
 <template>
   <v-container>
-    <v-layout
-      text-center
-      wrap
-    >
-      <v-flex xs12>
-        <v-img
-          :src="require('../assets/logo.svg')"
-          class="my-3"
-          contain
-          height="200"
-        ></v-img>
-      </v-flex>
-
-      <v-flex mb-4>
-        <h1 class="display-2 font-weight-bold mb-3">
-          Welcome to Vuetify
-        </h1>
-        <p class="subheading font-weight-regular">
-          For help and collaboration with other Vuetify developers,
-          <br>please join our online
-          <a href="https://community.vuetifyjs.com" target="_blank">Discord Community</a>
-        </p>
-      </v-flex>
-
-      <v-flex
-        mb-5
-        xs12
-      >
-        <h2 class="headline font-weight-bold mb-3">What's next?</h2>
-
-        <v-layout justify-center>
-          <a
-            v-for="(next, i) in whatsNext"
-            :key="i"
-            :href="next.href"
-            class="subheading mx-3"
-            target="_blank"
-          >
-            {{ next.text }}
-          </a>
-        </v-layout>
-      </v-flex>
-
-      <v-flex
-        xs12
-        mb-5
-      >
-        <h2 class="headline font-weight-bold mb-3">Important Links</h2>
-
-        <v-layout justify-center>
-          <a
-            v-for="(link, i) in importantLinks"
-            :key="i"
-            :href="link.href"
-            class="subheading mx-3"
-            target="_blank"
-          >
-            {{ link.text }}
-          </a>
-        </v-layout>
-      </v-flex>
-
-      <v-flex
-        xs12
-        mb-5
-      >
-        <h2 class="headline font-weight-bold mb-3">Ecosystem</h2>
-
-        <v-layout justify-center>
-          <a
-            v-for="(eco, i) in ecosystem"
-            :key="i"
-            :href="eco.href"
-            class="subheading mx-3"
-            target="_blank"
-          >
-            {{ eco.text }}
-          </a>
-        </v-layout>
-      </v-flex>
-    </v-layout>
+    <div v-show="Accueil"> 
+      Bienvenu sur Choupy, le jeu pour améliorer votre vocabulaire, 
+      vous allez devoir trouver des mots à partir de la définition qu'on vous donne, 
+      du nombre de lettres et du type (nom, adjectif ...). Vous aurez le droit à 2 erreurs,
+      quand vous vous trompez 2 fois sur le même mot, nous vous donnons la première lettre du mot en indice.
+      Bonne chance ! 
+    <p><v-btn @click="Page_Accueil"> Jouer </v-btn></p>
+    </div>
+    <div v-show ="!Accueil">
+    <p>La définition du mot est : {{Mots[n].def}}</p>
+    <p v-show="montrer_indice">La première du mot est : {{Mots[n].indice}}</p>
+    <p>Le mot est un : {{Mots[n].type}}</p>
+    <p>Le mot est composé de {{Mots[n].nbLettre}} lettres</p>
+    <v-btn @click="indentation">mot suivant</v-btn>
+    <v-text-field v-model="reponse" label="Entrez votre réponse">Entrez votre réponse</v-text-field>
+    </div>
   </v-container>
 </template>
 
 <script>
+
 export default {
   data: () => ({
-    ecosystem: [
+    Mots: [
       {
-        text: 'vuetify-loader',
-        href: 'https://github.com/vuetifyjs/vuetify-loader',
+        mot: 'tonitruant',
+        type: 'Adjectif',
+        nbLettre: 10,
+        def: 'Bruit de tonnerre, bruit énorme',
+        indice: 'T',
       },
       {
-        text: 'github',
-        href: 'https://github.com/vuetifyjs/vuetify',
+        mot: 'absurde',
+        type: 'Adjectif et nom masculin',
+        nbLettre: 7,
+        def: 'Contre raison, contre logique',
+        indice: 'A',
       },
       {
-        text: 'awesome-vuetify',
-        href: 'https://github.com/vuetifyjs/awesome-vuetify',
+        mot: 'sébum',
+        type: 'Nom masculin',
+        nbLettre: 5,
+        def: 'Sécrétion grasse produite par les glandes sébacées (la peau)',
+        indice: 'S',
       },
+      {
+        mot: 'iridescent',
+        type: 'Adjectif',
+        nbLettre: 10,
+        def: 'Qui a des reflets colorés de manière changeante',
+        indice: 'I',
+      },
+      {
+        mot: 'convexe',
+        type: 'Adjectif',
+        nbLettre: 7,
+        def: "Courbé, arrondi vers l'éxterieur",
+        indice: 'C',
+      },
+      {
+        mot: 'concave',
+        type:'Adjectif',
+        nbLettre: 7,
+        def: "Courbé, arrondi vers l'intérieur",
+        indice: 'C',
+      },
+      {
+        mot: 'anosognosie',
+        type:'Nom féminin',
+        nbLettre: 11,
+        def: "Trouble neuropsychique qui fait qu'un patient atteint d'une maladie ne semble pas avoir conscience de sa condition",
+        indice: 'A',
+      },
+      {
+        mot: 'effervescence',
+        type: 'Nom féminin',
+        nbLettre: 13,
+        def: "Bouillonnement d'un liquide par un dégagement de gaz. Peut également désigner une vive agitation",
+        indice: 'E',
+      },
+      {
+        mot: 'transcendant',
+        type: 'Adjectif',
+        nbLettre: 12,
+        def: "Qui s'élève au-dessus du niveau moyen",
+        indice: 'T',
+      },
+      {
+        mot: 'trivial',
+        type: 'Adjectif',
+        nbLettre: 7,
+        def: 'Ordinaire, commun',
+        indice: 'T',
+      },
+      {
+        mot: 'ordinal',
+        type: 'Adjectif',
+        nbLettre: 7,
+        def: 'qui désigne un rang',
+        indice: 'O'
+      },
+      {
+        mot: 'ascension',
+        type: 'Nom',
+        nbLettre: 8,
+        def: 'Elevation, action de gravir',
+        indice: 'A'
+      },
+      {
+        mot: 'eveil',
+        type: 'Nom',
+        nbLettre: 5,
+        def: "Tirer quelqu'un de son sommeil, rendre quelque chose effectif",
+        indice: 'E',
+      },
+      {
+        mot: 'draconien',
+        type:'Adjectif',
+        nbLettre: 9,
+        def: 'sévérité extrême',
+        indice: 'D',
+      }
+
     ],
-    importantLinks: [
-      {
-        text: 'Documentation',
-        href: 'https://vuetifyjs.com',
-      },
-      {
-        text: 'Chat',
-        href: 'https://community.vuetifyjs.com',
-      },
-      {
-        text: 'Made with Vuetify',
-        href: 'https://madewithvuejs.com/vuetify',
-      },
-      {
-        text: 'Twitter',
-        href: 'https://twitter.com/vuetifyjs',
-      },
-      {
-        text: 'Articles',
-        href: 'https://medium.com/vuetify',
-      },
-    ],
-    whatsNext: [
-      {
-        text: 'Explore components',
-        href: 'https://vuetifyjs.com/components/api-explorer',
-      },
-      {
-        text: 'Select a layout',
-        href: 'https://vuetifyjs.com/layout/pre-defined',
-      },
-      {
-        text: 'Frequently Asked Questions',
-        href: 'https://vuetifyjs.com/getting-started/frequently-asked-questions',
-      },
-    ],
+    n: 0,
+    reponse: '',
+    i: 0,
+    Accueil: true,
+    Jeux: false,
+    montrer_indice: false
   }),
+  methods: {
+
+    Page_Accueil (){
+      this.Accueil = false
+    },
+
+    indentation () {
+      if (this.reponse === this.Mots[this.n].mot) {
+        this.n = this.n +1;
+      } else {
+        this.i = this.i + 1;
+        if (this.i == 2) {
+          this.montrer_indice = true
+        }
+        }
+      console.log(this.i),
+      console.log(this.n),
+      console.log(this.reponse);
+    return ''
+    }
+  }
 };
 </script>
